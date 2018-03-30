0960d04c09b0d669f29bd2d61dac593114d6cb82
Dominic Cupo <Dominic.Cupo@gmail.com>
2018-03-29 20:37:02 -0400

    Added get_actual_movement static function
    This takes in a numeric representation of which move is wanted and determines what actually happens based on the provided probabilities. Returns a tuple of (move, is_double) where move matches the numeric representation of the input and is_double is a bool.

--- src/SARSA.py
+++ src/SARSA.py
@@ -1,3 +1,6 @@
+import random
+
+
 class SARSA:
     def __init__(self, goal_state_reward, pit_reward, move_reward,
                  give_up_reward, num_trails, exploration_epsilon):
@@ -8,6 +11,9 @@ class SARSA:
         self._num_trials = num_trails
         self._exploration_epsilon = exploration_epsilon
 
+        # randomize the seed to current system time
+        random.seed(None)
+
     def learn(self):
         for ii in range(self._num_trials):
             # do the trails, once we figure out how to do it
@@ -17,3 +23,33 @@ class SARSA:
 
     def print_recommended_actions(self):
         pass
+
+    '''
+    requested move = [0, 1, 2, 3] for [up, right, down, left]
+    returns (move, is_double) where 'move' is the same mapping as the input, and 'is_double' is a bool flag
+    '''
+    @staticmethod
+    def get_actual_movement(requested_move):
+
+        def rotate_right(wanted_move):
+            wanted_move += 1
+            if wanted_move == 4:
+                wanted_move = 0
+            return wanted_move
+
+        def rotate_left(wanted_move):
+            wanted_move -= 1
+            if wanted_move == -1:
+                wanted_move = 4
+            return wanted_move
+
+        val = random.randint(0, 10)
+
+        if val <= 7:
+            return requested_move, False
+        elif 7 < val <= 8:
+            return rotate_right(requested_move), False
+        elif 8 < val <= 9:
+            return rotate_left(requested_move), False
+        else:
+            return requested_move, True
